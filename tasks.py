f027048c502595678b76d93e8338fed20c9e602d
Mario Vago Marzal <mariovagomarzal@gmail.com>
2023-11-01 01:07:07 +0100

    Add support for the automation of tasks with 'Invoke'
    Add `invoke` and `pytest-doctestplus` dependencies and a `tasks.py` file with
    a simple task to run tests.

---
+++ tasks.py
@@ -0,0 +1,21 @@
+"""Invoke tasks."""
+from invoke import task
+
+
+# Constants
+PROJECT_NAME = "cdgtools"
+
+
+@task(incrementable=["verbose"])
+def tests(c, verbose=0, no_doctest=False):
+    """Run tests."""
+    verbose_level = "v" * verbose
+    if verbose_level:
+        pytest_command = f"pytest -{verbose_level}"
+    else:
+        pytest_command = "pytest"
+
+    if not no_doctest:
+        pytest_command += " --doctest-modules"
+
+    c.run(pytest_command, pty=True)
